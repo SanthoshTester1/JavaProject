1e2db63369f9332e4e707263d29cb7b30f40910b
SanthoshTester1 <gorantalasanthosh@gmail.com>
2024-06-05 21:05:36 +0530

    SwapnumberProgram

---
+++ src/main/java/FAQ/Programs/Swaptwonumber$1.java
@@ -0,0 +1,18 @@
+package FAQ.Programs;
+
+public class Swaptwonumber$1 {
+		public static void main(String[] args) {
+			int a = 20, b =10; 
+			
+			System.out.println("Before A Value:- " + a);
+			System.out.println("Before B Value:- " + b);
+			
+			int temp = a; 
+			a = b; 
+			b = temp;
+			
+			System.out.println("After A Value:- " + a);
+			System.out.println("After B Value:- " + b);
+				
+		}
+}
